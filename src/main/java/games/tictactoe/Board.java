abcb7538642a03ff47a761ad7cdbdbcb84a8298a
sumeet.g <sumeet.g@directi.com>
2017-07-13 19:40:44 +0530

    added board class

---
+++ src/main/java/games/tictactoe/Board.java
@@ -0,0 +1,14 @@
+package games.tictactoe;
+
+/**
+ * Created by sumeet
+ * on 13/7/17.
+ */
+public class Board {
+    private final boolean board[][];
+
+
+    public Board(int n) {
+        board = new boolean[n][n];
+    }
+}
