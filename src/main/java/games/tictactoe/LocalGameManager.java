7b758d2794886e98a4e00d641a6f43636fd79ea5
sumeet.g <sumeet.g@directi.com>
2017-07-13 23:16:58 +0530

    added localgamemanager

--- src/main/java/games/tictactoe/LocalGameManager.java
+++ src/main/java/games/tictactoe/LocalGameManager.java
@@ -1,5 +1,7 @@
 package games.tictactoe;
 
+import games.tictactoe.beans.Move;
+
 import java.io.IOException;
 
 /**
@@ -15,5 +17,13 @@ public class LocalGameManager extends GameManager {
     @Override
     protected void broadCastToPlayers(String message) throws IOException {
         player1.writeLine(message);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Player player1 = new Player(System.in, Move.X, System.out);
+        Player player2 = new Player(System.in, Move.O, System.out);
+        GameManager gameManager = new LocalGameManager(3, player1, player2);
+        gameManager.play();
+
     }
 }
